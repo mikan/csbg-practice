6c82be3e0851583b2748f37502854df31e2275f1
mikan <yutakakato@icloud.com>
2017-02-15 15:35:23 +0900

    プロジェクト ファイルを追加します。

---
+++ csbg-practice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csbg_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
